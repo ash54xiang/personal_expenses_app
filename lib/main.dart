d99d0690e0141881e91549afa301fece3d5b7607
Chin Xiang Seah <ash54xiang@gmail.com>
2022-10-12 15:10:54 +0800

    Force the app in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
 import './widgets/chart.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // The following lines of code is to force the app to be used in Portrait mode
+  // ----------
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  // ----------
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
